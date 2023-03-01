6c4b666ef7de9de493c3fd2d808abc2e56b6b2d9
root <6274@holbertonstudents.com>
2023-03-01 01:48:07 -0800

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* Returns 0 on successful execution */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int n;
