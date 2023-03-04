43ee990d15aa4f9980b4e55352e57a5daded2748
root <6274@holbertonstudents.com>
2023-03-04 04:54:10 -0800

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return :Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+while (n <= 48 && n >= 57)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
