b7a691e6a9c882bcf6f1d0d5767738e50b56626f
root <6274@holbertonstudents.com>
2023-03-04 04:43:18 -0800

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+while (n < 10)
+{
+printf("%i", n);
+n++;
+}
+putchar ('\n');
+return 0;
+}
