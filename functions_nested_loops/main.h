17d3cd6dddbbffb5014f64837b44a47857fc17a7
root <6274@holbertonstudents.com>
2023-03-05 03:39:01 -0800

    fix

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,15 +1,14 @@
-#include <main.h>
+#include <unistd.h>
+
 /**
-*main -Entry point
-*
-* Return: Always 0 (success)
-*/
-int main(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-char *str = "_putchar\n";
-while (*str)
-{
-_putchar(*str++);
+	return (write(1, &c, 1));
 }
-return (0);
-}
+
