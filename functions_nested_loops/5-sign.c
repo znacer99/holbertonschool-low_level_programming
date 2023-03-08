72f22c2c48e22c7a5e919c30f2e2d9a3bacb5c1d
root <6274@holbertonstudents.com>
2023-03-08 01:44:44 -0800

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int print_sign(int n)
+{
+if (n > 0)
+_putchar('+');
+{
+return (1);
+}
+if else
+_putchar('0');
+{
+return (0);
+}
+else
+_putchar('-');
+{
+return (-1);
+}
+}
