3ecec6b0df59ee5191c9087a40ba1fc93199eee3
root <6274@holbertonstudents.com>
2023-03-08 02:54:36 -0800

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit -Entry point
+*
+*
+*/
+
+int print_last_digit(int)
+
+{
+int last_digit = r % 10;
+_putchar('0' + last_digit);
+return (last_digit);
+}
