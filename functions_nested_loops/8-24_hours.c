5fadae43b77390d888c3ada5d665bb1fe6b63248
root <6274@holbertonstudents.com>
2023-03-09 12:15:27 -0800

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* jack_bauer - Entry point
+*
+* Return: value
+*/
+void times_table(void)
+{
+int h, m;
+
+for (h = 0; h < 24; h++)
+for (m = 0 ; m < 60; m++)		{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
+}
