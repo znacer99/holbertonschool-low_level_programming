a9abd27854f089e468b8ed7037c1cecf9d4e6ad4
root <6274@holbertonstudents.com>
2023-03-25 08:51:46 -0700

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - print number from 0-9
+*
+* Return: void
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+}
