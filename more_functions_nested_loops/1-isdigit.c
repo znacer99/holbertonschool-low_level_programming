ec6a513884315b2bb222e93d958375713c4c1348
root <6274@holbertonstudents.com>
2023-03-25 08:49:49 -0700

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _isdigit - check if c is a digit
+* @c: int
+* Return: if c is int
+*/
+
+int _isdigit(int c)
+{
+if (c > 47 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
