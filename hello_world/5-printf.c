4eac79a3938328b537c686b337e696b1639e5c23
root <6274@holbertonstudents.com>
2023-03-25 09:09:10 -0700

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,8 +3,8 @@
 * main - Entry point
 * * Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
