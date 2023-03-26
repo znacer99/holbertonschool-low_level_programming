056c41ddc19d1cf94f07bbf5c72c5c36ac949e65
root <6274@holbertonstudents.com>
2023-03-26 16:11:13 -0700

    4**

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,29 +1,25 @@
 #include "main.h"
+
 /**
- * _strspn - Entry point
+ * _strpbrk - Entry point
  * @s: input
  * @accept: input
  * Return: Always 0 (Success)
  */
 
-unsigned int _strspn(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	unsigned int n = 0;
-	int r;
+	int k;
 
 	while (*s)
 	{
-		for (r = 0; accept[r]; r++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
-			else if (accept[r + 1] == '\0')
-				return (n);
+			if (*s == accept[k])
+				return (s);
 		}
 		s++;
 	}
-	return (n);
+
+	return ('\0');
 }
