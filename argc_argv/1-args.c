2225c10cceef0cefbb787329c00a51c86b2fdf42
root <6274@holbertonstudents.com>
2023-04-03 10:20:48 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the number of argments passed into it
+* @argv: an array containing the programm command line arguments
+* @argc: N of command line arguments
+* Return: (0 - Success)
+*/
+
+int main(int argc, char *argv[])
+{
+
+}
