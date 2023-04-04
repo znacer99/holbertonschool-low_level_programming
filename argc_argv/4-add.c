60dd81756d095526224cea0c10848d08b7f22545
root <6274@holbertonstudents.com>
2023-04-04 03:24:16 -0700

    4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if successful, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+int i, j;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+puts("Error");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
