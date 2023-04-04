49d31c9d0455a0b2366a4b93ee7d157d50a48caf
root <6274@holbertonstudents.com>
2023-04-04 02:44:58 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -4,13 +4,12 @@
 
 int main(int argc, char *argv[])
 {
-if(argc = 2)
-printf("%s\n", argv[0]*argv[1]);
-else
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
 
