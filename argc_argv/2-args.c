3919da0b09659a728ec64f7a0d6d6b6fd1f95332
root <6274@holbertonstudents.com>
2023-04-04 02:09:53 -0700

    2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 /**
+* main - prints each argumet it receives in a single line
+* @argv: an array containing the programm command line arguments
+* @argc: N of command line arguments
+* Return: (0 - Success)
 */
 int main(int argc, char *argv[])
 {
