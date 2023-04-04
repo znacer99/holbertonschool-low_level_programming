dde3e1dbd2d54a25a975567828e9e70a4db81e25
root <6274@holbertonstudents.com>
2023-04-04 02:48:14 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+* main - prints the product of the multiplication between two numbers
+* @argv: an array containing the programm command line arguments
+* @argc: N of command line arguments
+* Return: 0 on Success, 1 if not given two numbers
 */
 
 int main(int argc, char *argv[])
