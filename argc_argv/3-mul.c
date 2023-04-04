337e49614cd8c40389e582a1068794c1b86c11f2
root <6274@holbertonstudents.com>
2023-04-04 02:21:27 -0700

    3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*/
+
+int main(int argc, char *argv[])
+{
+i = 0
+if(argc = 2)
+printf("%s\n", argv[0]*argv[1]);
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
+
