bd193dda0ae5f55da6c5ddb100cc8a48681867da
root <6274@holbertonstudents.com>
2023-04-17 02:22:01 -0700

    test

---
+++ structures_typedef/main_0.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Ghost";
+	my_dog.age = 4.75;
+	my_dog.owner = "Jon Snow";
+	print_dog(&my_dog);
+	return (0);
+}
