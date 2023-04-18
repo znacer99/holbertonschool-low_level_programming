12f9198f628218bbb5bb697e709ea65d9fdf3032
root <6274@holbertonstudents.com>
2023-04-17 17:48:40 -0700

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,25 +12,21 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-
-    new_dog->name = malloc(strlen(name) + 1);
-    if (new_dog->name == NULL) {
-        free(new_dog);
-        return (NULL);
-    }
-
-    new_dog->owner = malloc(strlen(owner) + 1);
-    if (new_dog->owner == NULL) {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
-
+dog_t *new_dog;
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+new_dog->name = malloc(strlen(name) + 1);
+if (new_dog->name == NULL) {
+free(new_dog);
+return (NULL);
+}
+new_dog->owner = malloc(strlen(owner) + 1);
+if (new_dog->owner == NULL) {
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
 strcpy(new_dog->name, name);
 new_dog->age = age;
 strcpy(new_dog->owner, owner);
