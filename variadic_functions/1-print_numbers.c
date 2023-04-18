607df8d20ab85595d5d92679446f7116f5e9c4db
root <6274@holbertonstudents.com>
2023-04-18 08:32:30 -0700

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,7 +6,6 @@
 *print_numbers - prints a variable number of integers
 *@separator: the string to be printed between integers
 *@n: the number of integers to print
-*
 @... : the integers to print
 *
 *Return: void
@@ -15,10 +14,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
+
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(args, int));
+}
 if (separator != NULL && i != n - 1)
 {
 printf("%s", separator);
