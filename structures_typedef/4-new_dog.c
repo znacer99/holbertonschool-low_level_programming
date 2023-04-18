0a3811622f48358c383ef72cfbe139a2c991c699
root <6274@holbertonstudents.com>
2023-04-17 17:43:18 -0700

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,53 +1,38 @@
 #include "dog.h"
-#include "string.h"
-#include "stdlib.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
-**new_dog - creating a function for a new dog
-*@name: a pointer
-*@age: a float
-*@owner: a pointer
-*Return: value of new_dog
-*/
-
+ * new_dog - creates a new dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog structure, or NULL on failure
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	int name_len, owner_len;
-	char *new_name, *new_owner;
+    dog_t *new_dog;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
+    new_dog = malloc(sizeof(dog_t)); // allocate memory for the new dog structure
+    if (new_dog == NULL) // check if memory allocation was successful
+        return (NULL);
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
+    new_dog->name = malloc(strlen(name) + 1); // allocate memory for the name string
+    if (new_dog->name == NULL) { // check if memory allocation was successful
+        free(new_dog);
+        return (NULL);
+    }
 
-	for (name_len = 0; name[name_len]; name_len++)
-		;
-	for (owner_len = 0; owner[owner_len]; owner_len++)
-		;
+    new_dog->owner = malloc(strlen(owner) + 1); // allocate memory for the owner string
+    if (new_dog->owner == NULL) { // check if memory allocation was successful
+        free(new_dog->name);
+        free(new_dog);
+        return (NULL);
+    }
 
-	new_name = malloc(sizeof(char) * (name_len + 1));
-	if (new_name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	new_owner = malloc(sizeof(char) * (owner_len + 1));
-	if (new_owner == NULL)
-	{
-		free(new_name);
-		free(new_dog);
-		return (NULL);
-	}
-
-	strcpy(new_name, name);
-	strcpy(new_owner, owner);
-
-	new_dog->name = new_name;
-	new_dog->age = age;
-	new_dog->owner = new_owner;
-
-	return (new_dog);
+strcpy(new_dog->name, name); // copy the name string to the new dog structure
+new_dog->age = age; // assign the age to the new dog structure
+strcpy(new_dog->owner, owner); // copy the owner string to the new dog structure
+return (new_dog); // return a pointer to the new dog structure
 }
