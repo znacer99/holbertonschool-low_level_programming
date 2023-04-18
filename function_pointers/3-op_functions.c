80af398268e456d131b2bd04d82979a08f3025d3
root <6274@holbertonstudents.com>
2023-04-18 03:18:04 -0700

    fixed

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ *op_add - returns the sum of a and b
+ *@a: an integer
+ *@b: an integer
+ *Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - returns the sum of a and b
+ *@a: an integer
+ *@b: an integer
+ *Return: sum of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - returns the sum of a and b
+ *@a: an integer
+ *@b: an integer
+ *Return: sum of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - returns the sum of a and b
+ *@a: an integer
+ *@b: an integer
+ *Return: sum of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - returns the sum of a and b
+ *@a: an integer
+ *@b: an integer
+ *Return: sum of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
