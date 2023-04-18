3d91e3091f31af0292ef88c16d68d41c853dbc61
root <6274@holbertonstudents.com>
2023-04-18 08:13:46 -0700

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@
 *
 *Return: void
 */
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
