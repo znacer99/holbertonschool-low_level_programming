8c41bd4505aeff55584928a311434231f5a7c91f
root <6274@holbertonstudents.com>
2023-04-28 10:10:58 -0700

    6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_delete - Deletes a hash table
+ *@ht: a pointer
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *node, *tmp;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
