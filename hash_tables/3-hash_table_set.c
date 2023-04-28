ea2191ec8bd9dfaad53f84caf103faf9f00e8c69
root <6274@holbertonstudents.com>
2023-04-28 10:19:55 -0700

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,6 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
 	hash_node_t *new_node = NULL, *temp_node = NULL;
+
 	if (!ht || !key || strlen(key) == 0)
 		return (0);
 
